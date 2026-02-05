15f67197f36fd9fe0645d88d95cb98f266afe3e0
AlexisARP <alexis.alejandro.ramos.palen.2006@calvino.edu.it>
2026-02-05 16:08:00 +0100

    Codice di prova

--- codice_del_nuovo_sensore/codice_del_nuovo_sensore.ino
+++ codice_del_nuovo_sensore/codice_del_nuovo_sensore.ino
@@ -1,124 +1,200 @@
 #include "FastIMU.h"
 #include <Wire.h>
 
-#define IMU_ADDRESS 0x69    //Change to the address of the IMU
-#define PERFORM_CALIBRATION //Comment to disable startup calibration
-BMI160 IMU;               //Change to the name of any supported IMU! 
 
-// Currently supported IMUS: MPU9255 MPU9250 MPU6886 MPU6500 MPU6050 ICM20689 ICM20690 BMI055 BMX055 BMI160 LSM6DS3 LSM6DSL QMI8658
+#define IMU_ADDRESS 0x68
+MPU6050 IMU;
 
-calData calib = { 0 };  //Calibration data
-AccelData accelData;    //Sensor data
+
+calData calib = {0};
+AccelData accelData;
 GyroData gyroData;
 MagData magData;
 
+
+// -------------------------
+// PIN LED + TILT
+// -------------------------
+const int ledPin = 2;
+const int tiltPin = 3;
+
+
+// -------------------------
+// PIN TEMPERATURA
+// -------------------------
+const int tempPin = A0;
+
+
+// -------------------------
+// INTERUTTORE A 3 POSIZIONI
+// -------------------------
+const int modeIMU = 6;     // Posizione 1
+const int modeLED = 7;     // Posizione 2
+const int modeTEMP = 8;    // Posizione 3
+
+
+unsigned long previousMillis = 0;
+const unsigned long interval = 3000;
+
+
+bool imuInitialized = false;   // Per evitare doppie inizializzazioni
+
+
 void setup() {
-  Wire.begin();
-  Wire.setClock(400000); //400khz clock
   Serial.begin(115200);
-  while (!Serial) {
-    ;
-  }
 
-  int err = IMU.init(calib, IMU_ADDRESS);
-  if (err != 0) {
-    Serial.print("Error initializing IMU: ");
-    Serial.println(err);
-    while (true) {
-      ;
-    }
-  }
-  
-#ifdef PERFORM_CALIBRATION
-  Serial.println("FastIMU calibration & data example");
-  if (IMU.hasMagnetometer()) {
-    delay(1000);
-    Serial.println("Move IMU in figure 8 pattern until done.");
-    delay(3000);
-    IMU.calibrateMag(&calib);
-    Serial.println("Magnetic calibration done!");
-  }
-  else {
-    delay(5000);
-  }
 
-  delay(5000);
-  Serial.println("Keep IMU level.");
-  delay(5000);
-  IMU.calibrateAccelGyro(&calib);
-  Serial.println("Calibration done!");
-  Serial.println("Accel biases X/Y/Z: ");
-  Serial.print(calib.accelBias[0]);
-  Serial.print(", ");
-  Serial.print(calib.accelBias[1]);
-  Serial.print(", ");
-  Serial.println(calib.accelBias[2]);
-  Serial.println("Gyro biases X/Y/Z: ");
-  Serial.print(calib.gyroBias[0]);
-  Serial.print(", ");
-  Serial.print(calib.gyroBias[1]);
-  Serial.print(", ");
-  Serial.println(calib.gyroBias[2]);
-  if (IMU.hasMagnetometer()) {
-    Serial.println("Mag biases X/Y/Z: ");
-    Serial.print(calib.magBias[0]);
-    Serial.print(", ");
-    Serial.print(calib.magBias[1]);
-    Serial.print(", ");
-    Serial.println(calib.magBias[2]);
-    Serial.println("Mag Scale X/Y/Z: ");
-    Serial.print(calib.magScale[0]);
-    Serial.print(", ");
-    Serial.print(calib.magScale[1]);
-    Serial.print(", ");
-    Serial.println(calib.magScale[2]);
-  }
-  delay(5000);
-  IMU.init(calib, IMU_ADDRESS);
-#endif
+  // MKR1010 usa SDA=11, SCL=12 automaticamente
+  Wire.begin();
 
-  //err = IMU.setGyroRange(500);      //USE THESE TO SET THE RANGE, IF AN INVALID RANGE IS SET IT WILL RETURN -1
-  //err = IMU.setAccelRange(2);       //THESE TWO SET THE GYRO RANGE TO ±500 DPS AND THE ACCELEROMETER RANGE TO ±2g
-  
-  if (err != 0) {
-    Serial.print("Error Setting range: ");
-    Serial.println(err);
-    while (true) {
-      ;
-    }
-  }
+
+  // LED + Tilt
+  pinMode(ledPin, OUTPUT);
+  pinMode(tiltPin, INPUT_PULLUP);
+
+
+  // Interruttore 3 posizioni
+  pinMode(modeIMU, INPUT_PULLUP);
+  pinMode(modeLED, INPUT_PULLUP);
+  pinMode(modeTEMP, INPUT_PULLUP);
 }
+
 
 void loop() {
+
+
+  // -------------------------
+  // MODALITÀ SELEZIONATA
+  // -------------------------
+  if (digitalRead(modeIMU) == LOW) {
+    runIMU();
+  }
+  else if (digitalRead(modeLED) == LOW) {
+    imuInitialized = false;  // Reset IMU quando si esce dalla modalità
+    runLEDandTilt();
+  }
+  else if (digitalRead(modeTEMP) == LOW) {
+    imuInitialized = false;
+    runTemperature();
+  }
+}
+
+
+// -------------------------
+// FUNZIONE 1: LETTURA IMU + CALIBRAZIONE
+// -------------------------
+void runIMU() {
+
+
+  // Esegui la calibrazione SOLO la prima volta che entri in modalità IMU
+  if (!imuInitialized) {
+
+
+    Serial.println("Inizializzazione IMU...");
+    int err = IMU.init(calib, IMU_ADDRESS);
+    if (err != 0) {
+      Serial.print("Errore inizializzazione IMU: ");
+      Serial.println(err);
+      return;
+    }
+
+
+    // --- CALIBRAZIONE COMPLETA ---
+    if (IMU.hasMagnetometer()) {
+      Serial.println("Muovi l'IMU a 8 per la calibrazione magnetica...");
+      delay(3000);
+      IMU.calibrateMag(&calib);
+      Serial.println("Calibrazione magnetica completata!");
+    }
+
+
+    Serial.println("Mantieni l'IMU ferma e in piano...");
+    delay(5000);
+    IMU.calibrateAccelGyro(&calib);
+    Serial.println("Calibrazione accelerometro/giroscopio completata!");
+
+
+    // Re-inizializza con i valori calibrati
+    IMU.init(calib, IMU_ADDRESS);
+
+
+    imuInitialized = true;
+    Serial.println("IMU pronta!");
+  }
+
+
+  // --- LETTURA DATI IMU ---
   IMU.update();
   IMU.getAccel(&accelData);
-  Serial.print(accelData.accelX);
-  Serial.print("\t");
-  Serial.print(accelData.accelY);
-  Serial.print("\t");
-  Serial.print(accelData.accelZ);
-  Serial.print("\t");
   IMU.getGyro(&gyroData);
-  Serial.print(gyroData.gyroX);
-  Serial.print("\t");
-  Serial.print(gyroData.gyroY);
-  Serial.print("\t");
+
+
+  Serial.print("ACC: ");
+  Serial.print(accelData.accelX); Serial.print("\t");
+  Serial.print(accelData.accelY); Serial.print("\t");
+  Serial.print(accelData.accelZ); Serial.print("\t");
+
+
+  Serial.print("GYR: ");
+  Serial.print(gyroData.gyroX); Serial.print("\t");
+  Serial.print(gyroData.gyroY); Serial.print("\t");
   Serial.print(gyroData.gyroZ);
+
+
   if (IMU.hasMagnetometer()) {
     IMU.getMag(&magData);
-    Serial.print("\t");
-    Serial.print(magData.magX);
-    Serial.print("\t");
-    Serial.print(magData.magY);
-    Serial.print("\t");
+    Serial.print("\tMAG: ");
+    Serial.print(magData.magX); Serial.print("\t");
+    Serial.print(magData.magY); Serial.print("\t");
     Serial.print(magData.magZ);
   }
+
+
   if (IMU.hasTemperature()) {
-	  Serial.print("\t");
-	  Serial.println(IMU.getTemp());
+    Serial.print("\tTEMP IMU: ");
+    Serial.print(IMU.getTemp());
   }
-  else {
-    Serial.println();
-  }
+
+
+  Serial.println();
   delay(50);
 }
+
+
+// -------------------------
+// FUNZIONE 2: LED + TILT
+// -------------------------
+void runLEDandTilt() {
+  if (digitalRead(tiltPin) == LOW) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
+
+
+// -------------------------
+// FUNZIONE 3: TEMPERATURA LM35
+// -------------------------
+void runTemperature() {
+  unsigned long currentMillis = millis();
+
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+
+    int val = analogRead(tempPin);
+    float voltage = val * (3.3 / 1024.0);   // MKR1010 = 3.3V
+    float temperature = voltage * 100.0;    // LM35: 10mV per °C
+
+
+    Serial.print("Temp LM35: ");
+    Serial.print(temperature);
+    Serial.println(" C");
+  }
+}
+
+
+
+
